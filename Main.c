22e916733c5f46129ecf44dc4d872e1b2c3bfd77
202255605 <102909920+202255605@users.noreply.github.com>
2022-11-08 08:52:52 +0900

    Add files via upload

--- Main.c
+++ Main.c
@@ -1,22 +1,34 @@
 #include <stdio.h> 
 
-int main(void) {
-	unsigned int b, e, i;
-	unsigned int x, y, sum=0;
+int main(void)
+{
+	int a[10]={11,22,33,44,55};
+	int i, j, n=5;
 
-	scanf("%d%d", &b, &e);
-	scanf("%d%d", &x, &y);
+	scanf("%d", &j);
+	// Enter your code here
+ 	if(j>=5) j=n;
+	if(j<1) j=n;
 	
-	// Enter your code
-	for(i=b;i<=e;i=i+1){
-	if(i%x==0||i%y==0) {
-		printf("%d ",i);
-		//printf("%d",i);
-		sum=sum+i;}
+	int b[5],number,number_2,account;
+	
+	for(number=0;number<j-1;number++){
+		b[number]=a[number];//0-(j-2) 즉 개수는 j-1개
 	}
 
+	for(number_2=j;number_2<=4;number_2++){
+		b[number_2-1]=a[number_2];//인자가 j-1부터 다 들어온다.
+	}
 
+	for(account=j;account<=4;account++){
+		a[account-1]=b[account-1];
+	}
 	
-	printf("\nsum == %d\n", sum);
+	n=4;
+
+	for(i=0;i<n;++i) 
+		printf("%d ", a[i]);
+	printf("\n");
+	
 	return 0;
 }
